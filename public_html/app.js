d7b32489c3f8aa812df3f4678f6187545a83d66d
Evan Nichols <33175950+Reccho@users.noreply.github.com>
2021-03-20 21:43:23 +0100

    Skeleton app.js
    Has basic button activations & mediaRecorder to record audio from user's microphone

---
+++ public_html/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+//methods
+	
+	function selectPhrase() {
+		$('phrase').val('');
+		
+		max_val = 0;
+		//REQUEST: Total number of phrases in xml document
+
+		//Check if phrase # is out of bounds and reset accordingly
+
+		//REQUEST: Fetch phrase text form xml, search by 'id'
+	}
+	
+	$(document).on("input", "#phraseNumber", selectPhrase);
+
+	//main
+	if (navigator.mediaDevices) {
+		var audio = { audio: true };
+		var chunks = [];
+		var blob = null;
+		var clipName = "";
+
+		navigator.mediaDevices.getUserMedia(audio).then(function (stream) {
+				var mediaRecorder = new MediaRecorder(stream);	// Init
+
+				// On-Click: "Record"
+				$("#record").click(function () {
+					$("#playback").trigger("stop");	// Stop playing preview
+					chunks = [];					// Set chunks empty
+					mediaRecorder.start();			// Start recording
+
+					//Button updates
+					$("#stop").attr("disabled", false);			//enable
+					$("#grade").attr("disabled", true);			//disable
+					$("#record").attr("disabled", true);		//disable
+					//$("#nextPhrase").attr("disabled", true);	//disable
+				});
+
+				// On-Click: "Stop"
+				$("#stop").click(function () {
+					mediaRecorder.stop();	// Stop recording
+
+					//Button updates
+					//$("#nextPhrase").attr("disabled", false);	//enable
+					$("#record").attr("disabled", false);		//enable
+					$("#grade").attr("disabled", false);		//enable
+					$("#stop").attr("disabled", true);			//disable
+				});
+
+				// On-Click: "Grade"
+				$("#grade").click(function() {
+					score = 0;
+					//REQUEST: Pass audio to grading function and get score value (decimal)
+				});
+
+				mediaRecorder.onstop = function (e) {
+					clipName = $("#dataset").val() + "_" + $("#phraseNumber").val();
+					blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+					chunks = [];
+					var audioURL = URL.createObjectURL(blob);
+					$("#preview").attr("src", audioURL);
+
+					if ($("#autoplay").is(":checked"))
+						$("#preview").trigger("play");
+				}
+
+				mediaRecorder.ondataavailable = function (e) {
+					chunks.push(e.data);
+				}
+
+			})
+			.catch(function (err) {
+				alert('Error encountered: ' + err);
+			})
+	}
+});
