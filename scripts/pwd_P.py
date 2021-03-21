24d4a658abf21e76f8e2f034b3c3fdf7f4510ae2
Evan Nichols <33175950+Reccho@users.noreply.github.com>
2021-03-21 15:33:42 +0100

    Function-Command: pwd -P

---
+++ scripts/pwd_P.py
@@ -0,0 +1,17 @@
+import os, sys, subprocess
+
+#Run 'pwd -P' on file "filename"
+def pwd_P(filename):
+    cmd = "pwd -P " + filename
+    sub = subprocess.Popen(cmd, 
+        shell=True, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, 
+        #universal_newlines=True
+        )
+    sox = sub.communicate()[0]
+    return(sox.decode().replace('\n', '') + "/" + filename)
+
+text = sys.argv[1]
+pwd = pwd_P(text)
+print(pwd)
