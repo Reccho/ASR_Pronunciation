7405c173887817e15245792f0b8c130399d3ef80
Evan Nichols <33175950+Reccho@users.noreply.github.com>
2021-05-25 03:10:47 +0200

    separate ASR and APP, code restructure

--- App/app.py
+++ App/app.py
@@ -1,52 +1,45 @@
 from flask import Flask, render_template, make_response, request
 from flask_cors import CORS, cross_origin #bypassing CORS locally
-import os, os.path, sys, stat, subprocess, signal, time, random, librosa, librosa.display, wave, contextlib, shutil, base64
+import os, os.path, sys, stat, subprocess, signal, time, random, base64, uuid
+import librosa, librosa.display, wave, contextlib, shutil
 import xml.etree.ElementTree as ET #XML tree toolkit
 import IPython.display as ipd
 import matplotlib.pyplot as plt
-import torch
-from argparse import ArgumentParser
-from nemo.collections.asr.metrics.wer import WER, word_error_rate
-from nemo.collections.asr.models import EncDecCTCModel
-from nemo.utils import logging
 
 LibPath = "/home/nichols/sw_project/lib/"
+thisPath = "/home/nichols/sw_project/"
+userID = ''
 
+#region XML Library interactions (phrase fetching)
 #Search xml file for phrase by id and return text string
 def phrase_Get(filename, itemNum):
-    tree = ET.parse(LibPath + filename)   # Create tree from xml file
-    root = tree.getroot()                                                           # Start at root
-    text = root.find('.//phrase[@id="{value}"]'.format(value=itemNum)).text         # Search for matching id, pull text
+    tree = ET.parse(LibPath + filename)                                     # Create tree from xml file
+    root = tree.getroot()                                                   # Begin at root of the tree
+    text = root.find('.//phrase[@id="{value}"]'.format(value=itemNum)).text # Search for matching id, return text
     return text
 
 #Return total number of phrases in xml file
 def phrase_Num(filename):
-    tree = ET.parse(LibPath + filename)   # Create tree from xml file
-    root = tree.getroot()
-    return len(root.findall('.//phrase'))
+    tree = ET.parse(LibPath + filename)     # Create tree from xml file
+    root = tree.getroot()                   # Begin at root of the tree
+    return len(root.findall('.//phrase'))   # Find all "phrase" -> return count
 
 #Return .xml files in library directory
 def getDatasets(path, ext):
     #Get list of all files and trim non-.xml documents
     return (f for f in os.listdir(path) if f.endswith('.' + ext))
 
-#Create Spectrogram
-def Spectro(filename):
-    plt.figure(figsize=(15,4))
-    data1,sample_rate1 = librosa.load(filename, sr=22050, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
-    #librosa.display.waveplot(data1,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
-    fig = plt.Figure()          #create spectrogram figure
-    #plt.draw()
-    X = librosa.stft(data1)
-    Xdb = librosa.amplitude_to_db(abs(X))
-    plt.figure(figsize=(14, 5))
-    librosa.display.specshow(Xdb, sr=sample_rate1, x_axis='time', y_axis='hz')
-    plt.colorbar()
+#endregion
 
-    plt.savefig('/home/nichols/sw_project/temp/spectro.png')  #save spectrogram to image file
-    os.chmod("/home/nichols/sw_project/temp/spectro.png", 0o755)   #RWX, RX, RX
-    #plt.show()
-    plt.close()
+#region Shell Commands via Python
+#Clear files that begin w/ uuid + spectro.png
+def clear_Directory(directory, id):
+    print(directory + userID + "*")     #TEST
+    cmd1 = "rm " + directory + id + "*"     
+    cmd2 = "rm " + directory + "spectro.png"
+    subprocess.run(cmd1, shell=True)
+    subprocess.run(cmd2, shell=True)
+    return
 
 #Phonemize "phrase" to get phonetic representation
 def phonemize(phrase):
@@ -82,6 +75,26 @@ def pwd_P(filename):
     sox = sub.communicate()[0]
     return(sox.decode().replace('\n', '') + "/" + filename)
 
+#endregion
+
+#Create Spectrogram
+def Spectro(audioFile, imageFile):
+    plt.figure(figsize=(15,4))
+    data1,sample_rate1 = librosa.load(audioFile, sr=22050, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+    #librosa.display.waveplot(data1,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+    fig = plt.Figure()          # create spectrogram figure
+    #plt.draw()
+    X = librosa.stft(data1)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr=sample_rate1, x_axis='time', y_axis='hz')
+    plt.colorbar()
+
+    plt.savefig(imageFile)      # save spectrogram to image file
+    os.chmod(imageFile, 0o755)  # O-RWX, G-RX, U-RX
+    #plt.show()
+    plt.close()
+
 #Return duration of audio file 'filename'
 def audio_Duration(filename):
     with contextlib.closing(wave.open(filename,'r')) as f:
@@ -98,100 +111,13 @@ def audio_Reformat(input, output):
     
 #Prepare the dataset to be fed to ASR
 def prep_Dataset(filename, duration, text):
-    DS = open(r'/home/nichols/sw_project/dataset/dataset.json','w')
+    #DS = open(thisPath + 'dataset/dataset.json','w')
+    DS = open(thisPath + 'temp/' + userID + '_dataset.json','w')
     contents = '{"audio_filename": "' + filename + '", "duration": ' + str(duration) + ', "text": "' + text + '"}'
     DS.write(contents)
     DS.close()
     return
 
-#Run the ASR on 'dataset.json', return score
-def ASR_Grade(dataset):
-    try:
-        from torch.cuda.amp import autocast
-    except ImportError:
-        from contextlib import contextmanager
-
-        @contextmanager
-        def autocast(enabled=None):
-            yield
-
-    can_gpu = torch.cuda.is_available()
-
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--asr_model",
-        type=str,
-        default="QuartzNet15x5Base-En",
-        required=True,
-        help="Pass: 'QuartzNet15x5Base-En'",
-    )
-    parser.add_argument(
-        "--dataset", type=str, required=True, help="path to evaluation data"
-    )
-    parser.add_argument("--batch_size", type=int, default=4)
-    parser.add_argument("--wer_tolerance", type=float, default=1.0, help="used by test")
-    parser.add_argument(
-        "--normalize_text",
-        default=True,
-        type=bool,
-        help="Normalize transcripts or not. Set to False for non-English.",
-    )
-    # args = parser.parse_args(["--dataset", "dataset.json", "--asr_model", "QuartzNet15x5Base-En"])
-    args = parser.parse_args(
-        ["--dataset", dataset, "--asr_model", "QuartzNet15x5Base-En"]
-    )
-    torch.set_grad_enabled(False)
-
-    if args.asr_model.endswith(".nemo"):
-        logging.info(f"Using local ASR model from {args.asr_model}")
-        asr_model = EncDecCTCModel.restore_from(restore_path=args.asr_model)
-    else:
-        logging.info(f"Using NGC cloud ASR model {args.asr_model}")
-        asr_model = EncDecCTCModel.from_pretrained(model_name=args.asr_model)
-    asr_model.setup_test_data(
-        test_data_config={
-            "sample_rate": 16000,
-            "manifest_filepath": args.dataset,
-            "labels": asr_model.decoder.vocabulary,
-            "batch_size": args.batch_size,
-            "normalize_transcripts": args.normalize_text,
-        }
-    )
-    if can_gpu:  # noqa
-        asr_model = asr_model.cuda()
-    asr_model.eval()
-    labels_map = dict(
-        [
-            (i, asr_model.decoder.vocabulary[i])
-            for i in range(len(asr_model.decoder.vocabulary))
-        ]
-    )
-    wer = WER(vocabulary=asr_model.decoder.vocabulary)
-    hypotheses = []
-    references = []
-    for test_batch in asr_model.test_dataloader():
-        if can_gpu:
-            test_batch = [x.cuda() for x in test_batch]
-        with autocast():
-            log_probs, encoded_len, greedy_predictions = asr_model(
-                input_signal=test_batch[0], input_signal_length=test_batch[1]
-            )
-        hypotheses = wer.ctc_decoder_predictions_tensor(greedy_predictions)
-        for batch_ind in range(greedy_predictions.shape[0]):
-            reference = "".join(
-                [labels_map[c] for c in test_batch[2][batch_ind].cpu().detach().numpy()]
-            )
-            references.append(reference)
-        del test_batch
-    wer_value = word_error_rate(hypotheses=hypotheses, references=references)
-
-    for h, r in zip(hypotheses, references):
-        print("Recognized:\t{}\nReference:\t{}\n".format(h, r))
-
-    logging.info(f"Got WER of {wer_value}. Tolerance was {args.wer_tolerance}")
-
-    score = 100.00 - (round((wer_value / args.wer_tolerance), 4) * 100)
-    return score
 
 
 #app._static_folder = os.path.abspath("static/")
@@ -200,58 +126,59 @@ CORS(app)
 
 @app.route('/')
 def index():
+    global userID
+    userID = uuid.uuid4().hex
+    print('ID: ' + userID)  #TEST
     return render_template('index.html')
 
 @app.route('/Library', methods=['POST']) #ajax uses GET by def.
 def query():
-    action = request.form.get('action', '')     # default '' if no "action" data
-
-    if action == "numPhrase":         # get phrase number --> return string
-        idD = request.form['idDataset']
-        return str(phrase_Num(idD))
-    elif action == "getPhrase":         # get phrase number --> return string
-        idP = request.form['idPhrase']
-        idD = request.form['idDataset']
-        phrase = phrase_Get(idD, idP)
-        return phrase
-    elif action == "getDatasets":       # get dataset name --> return dataset title, open xml tree
-        files = getDatasets(LibPath, "xml")
-        filesStr = " "
-        return (filesStr.join(files))
-    return ":)"
+    action = request.form.get('action', '') # default '' if no "action" data
+    if action == "numPhrase":   # Number of phrases in dataset
+        idD = request.form['idDataset'] # Extract dataset ID
+        return str(phrase_Num(idD))     # Return # of phrases in that dataset
+    elif action == "getPhrase": # get phrase --> return text
+        idP = request.form['idPhrase']  # Extract phrase ID
+        idD = request.form['idDataset'] # Extract dataset ID
+        phrase = phrase_Get(idD, idP)   # Find phrase (by id) in dataset
+        return phrase                   # return string
+    elif action == "getDatasets":   # Get available datasets
+        files = getDatasets(LibPath, "xml") # Get list of .xml files in library
+        filesStr = " "                      # Delimiter
+        return (filesStr.join(files))       # Return list of library files
+    return ":)" # Default (probably not needed)
 
 @app.route('/Store_Phrase', methods=['POST']) #ajax uses GET by def.
 def storePhrase():    # write Phrase to sample.txt
-    #Clear 'temp' folder contents first
-    '''
-    folder = '/home/nichols/sw_project/temp'
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-    '''
-    phrase = request.form['text']
-    storeTxt = open(r'/home/nichols/sw_project/dataset/sample.txt','w')
-    storeTxt.write(phrase)
-    storeTxt.close()
-    os.chmod('/home/nichols/sw_project/dataset/sample.txt', 0o755)   #RWX, RX, RX
-    return "Phrase -> 'sample.txt'"
+    sampleFile = thisPath + 'temp/' + userID + '_sample.txt'  # Path for sample.txt
+    phrase = request.form['text']       # Extract phrase string
+    storeTxt = open(sampleFile,'w')     # Create file to hold phrase
+    storeTxt.write(phrase)              # Write phrase to file
+    storeTxt.close()                    # Close file writing
+    os.chmod(sampleFile, 0o755)         # O-RWX, G-RX, U-RX
+    return "Phrase -> 'sample.txt'"     # "confirmation" message
 
 @app.route('/Store_Audio', methods=['POST']) #ajax uses GET by def.
-def storeAudio():    # write Audio to input.wav
-    audioFile = "/home/nichols/sw_project/temp/input.wav"
+def storeAudio():    # write Audio to input.wav, assemble dataset for ASR, create spectrogram
+    datasetFile = thisPath + 'temp/' + userID + '_sample.wav'   # Path for sample.wav
+    sampleFile = thisPath + 'temp/' + userID + '_sample.txt'    # Path for sample.txt
+    audioFile = thisPath + 'temp/' + userID + '_input.wav'      # Path for input.wav
     
-    f = open(audioFile, 'wb')   #create file for input
-    f.write(request.data)           #write audio to file
-    f.close()                       #
-    #
-    Spectro(audioFile)
-    with open("/home/nichols/sw_project/temp/spectro.png", "rb") as png:
+    f = open(audioFile, 'wb')   # create file for input
+    f.write(request.data)       # write audio to file
+    f.close()                   # close file
+
+    phrase_file = open(sampleFile, "r") # open phrase text file
+    phrase = phrase_file.read()         # read phrase text string
+    phrase_file.close()                 # close file
+    
+    audio_Reformat(audioFile, datasetFile)      # Reformat audio
+    duration = audio_Duration(datasetFile)      # Get duration of audio
+    prep_Dataset(datasetFile, duration, phrase) # Create dataset.json
+    os.chmod(datasetFile, 0o755)                # O-RWX, G-RX, U-RX
+    
+    Spectro(audioFile, thisPath + 'temp/spectro.png')
+    with open(thisPath + "temp/spectro.png", "rb") as png:
         image_binary = png.read()
         response = make_response(base64.b64encode(image_binary))
         response.headers.set('Content-Type', 'image/gif')
@@ -259,18 +186,24 @@ def storeAudio():    # write Audio to input.wav
     return response
 
 @app.route('/Grade', methods=['POST']) #ajax uses GET by def.
-def grade():    # get audio --> return grade    
-    phrase_file = open("/home/nichols/sw_project/dataset/sample.txt", "r")
-    phrase = phrase_file.read()                                 # get phrase text
-    phrase_file.close()
-
-    audio_Reformat("/home/nichols/sw_project/temp/input.wav", "/home/nichols/sw_project/dataset/sample.wav")     # Reformat audio
-    duration = audio_Duration("/home/nichols/sw_project/dataset/sample.wav")               # Get duration of audio
-    prep_Dataset("/home/nichols/sw_project/dataset/sample.wav", duration, phrase)         # Create dataset.json
+def grade():    # Read results file & return strings
+    resultsFile = '/home/nichols/sw_project/temp/' + userID + '_graded.txt'
     
-    score = ASR_Grade("/home/nichols/sw_project/dataset/dataset.json")
-    #score = (round((random.random()), 4) * 100)
-    return str(score)
+    if os.path.exists(resultsFile): 
+        with open(resultsFile) as my_file:
+            graded = my_file.readlines()
+        transcript = graded[0]
+        phonemes = graded[1]
+        score = graded[2]
+        return (transcript + '\n' + phonemes + '\n' + score)
+    else:
+        return ('XXX')
+
+@app.route('/Clear', methods=['POST']) #ajax uses GET by def.
+def Clear():    # clear /temp/ and /dataset
+    Temp_dir = thisPath + 'temp'
+    clear_Directory(thisPath + 'temp/', userID)
+    return("cleared.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
